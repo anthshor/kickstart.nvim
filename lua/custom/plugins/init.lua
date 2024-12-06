df8f5c71e30038e6092a46aaf041115d994f93b5
Anthony Shorter <anthshor@gmail.com>
2024-12-06 21:41:48 +0000

    Add ChatGPT plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,21 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+-- Lazy
+return {
+  'jackMort/ChatGPT.nvim',
+  event = 'VeryLazy',
+  config = function()
+    local chatgpt = require 'chatgpt'
+    chatgpt.setup()
+    vim.keymap.set('v', '<leader>pe', function()
+      chatgpt.edit_with_instructions()
+    end, { desc = 'ChatGPT: Edit with instructions' })
+  end,
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+    'nvim-lua/plenary.nvim',
+    'folke/trouble.nvim', -- optional
+    'nvim-telescope/telescope.nvim',
+  },
+}
