bc50983f1711eb5b3ddd20e1e1f4d3969b23cf11
Anthony Shorter <anthshor@gmail.com>
2024-12-17 20:47:58 +0000

    Add buffer keymaps

--- init.lua
+++ init.lua
@@ -196,6 +196,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Buffer next and previous
+vim.keymap.set('n', '<Leader>bn', ':bnext<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>bp', ':bprevious<CR>', { noremap = true, silent = true })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
